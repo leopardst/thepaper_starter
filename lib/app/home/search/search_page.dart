6afa8110ae777eaaca96a8d90cf626c4d38343af
leopardst <velibor.santic1@gmail.com>
2021-08-04 10:56:38 -0400

    fixed search

--- lib/app/home/search/search_page.dart
+++ lib/app/home/search/search_page.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:algolia/algolia.dart';
 import 'package:provider/provider.dart';
 import 'package:thepaper_starter/app/home/search/search_list_tile.dart';
 import 'package:thepaper_starter/services/analytics_service.dart';
-
+import 'package:flappy_search_bar/flappy_search_bar.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -45,21 +44,20 @@ class _SearchPageState extends State<SearchPage>{
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Container(),
-          // child: SearchBar<AlgoliaObjectSnapshot>(
-          //   onSearch: search2,
-          //   hintText: "Search",
-          //   hintStyle: TextStyle(
-          //     color: Colors.grey[600],
-          //   ),
-          //   emptyWidget: Padding(
-          //     padding: const EdgeInsets.only(top: 4.0),
-          //     child: Text("No results found"),
-          //   ),
-          //   onItemFound: (AlgoliaObjectSnapshot snap, int index){
-          //     return SearchListTile(snap: snap, index: index);
-          //   },
-          // ),
+          child: SearchBar<AlgoliaObjectSnapshot>(
+            onSearch: search2,
+            hintText: "Search",
+            hintStyle: TextStyle(
+              color: Colors.grey[600],
+            ),
+            emptyWidget: Padding(
+              padding: const EdgeInsets.only(top: 4.0),
+              child: Text("No results found"),
+            ),
+            onItemFound: (AlgoliaObjectSnapshot snap, int index){
+              return SearchListTile(snap: snap, index: index);
+            },
+          ),
         ),
       ),
     );
